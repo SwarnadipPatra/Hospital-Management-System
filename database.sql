8f1cc5457fa45b46ae295acb4a8dcf380d71c59f
Swarnadip Patra <swarnadippatra1812@gmail.com>
2026-01-07 21:09:10 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS hospital;
+
+CREATE DATABASE hospital;
+
+Use hospital;
+
+CREATE TABLE patients(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    gender VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE doctors(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    specialization VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE appointments(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    patient_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    appointment_date DATE NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(id)
+);
+
+INSERT INTO doctors (name, specialization) VALUES
+('Pankaj Jain', 'Physician'),
+('Alok Das', 'NeuroSurgeon');
+
